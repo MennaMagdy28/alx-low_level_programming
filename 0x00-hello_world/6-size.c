48c614d15f555d8c46c85502c040b3e9ff7a33fb
Menna Magdy <menna11hassanien@gmail.com>
2023-07-13 07:23:46 -0700

    k

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main-start
+* c size
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+printf("Size of a char:lu\n", sizeof(char));
+printf("Size of a int:lu\n", sizeof(int));
+printf("Size of a long int:lu\n", sizeof(long int));
+printf("Size of a long long int:lu\n", sizeof(long long int));
+printf("Size of a float:lu\n", sizeof(float));
+return (0);
+}
