4133f1d8e13038a12e1debb860e7bf9fc3e56946
Menna Magdy <menna11hassanien@gmail.com>
2023-07-13 08:52:10 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1, string, 59);
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, str, 59);
 return (1);
 }
