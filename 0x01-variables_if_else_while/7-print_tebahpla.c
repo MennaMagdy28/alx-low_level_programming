e44cfa766eab3cbe82005b0c5eeaeb5d8d12bc87
Menna Magdy <menna11hassanien@gmail.com>
2023-07-14 20:18:23 +0300

    k

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
 *main - started
-*Description: 
-*Return:
+*Description: wirte alpahbet with putchar reversed
+*Return:Always 0 (success)
 */
 int main(void)
 {
-
+char a;
+for (a = 122; a >= 97; a--)
+putchar(a);
+putchar('\n');
+return (0);
 }
+
