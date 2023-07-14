627087de49c8db8935caf94a35ada7456496b3d0
Menna Magdy <menna11hassanien@gmail.com>
2023-07-14 20:12:04 +0300

    k

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
 *main - started
-*Description: 
-*Return:
+*Description: print numbers 0:9
+*Return:Always 0 (success)
 */
 int main(void)
 {
-
+int a;
+for (a = 0; a <= 9; a++)
+printf("%d", a);
+putchar('\n');
+return (0);
 }
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
 *main - started
-*Description: 
-*Return:
+*Description: wirte numbers with putchar
+*Return:Always 0 (success)
 */
 int main(void)
 {
-
+char a;
+for (a = 48; a <= 57; a++)
+putchar(a);
+putchar('\n');
+return (0);
 }
