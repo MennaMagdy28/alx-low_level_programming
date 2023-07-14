16d1920593a1df464e9544c165992b139196a3fa
Menna Magdy <menna11hassanien@gmail.com>
2023-07-14 22:42:35 +0300

    k

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,11 +7,14 @@
 */
 int main(void) {
 int i, j;
-for (i = 0; i < 9; i++) {
-for (j = i + 1; j <= 9; j++) {
+for (i = 0; i < 9; i++)
+{
+for (j = i + 1; j <= 9; j++)
+{
 putchar(i + '0');
 putchar(j + '0');
-if (i != 8 || j != 9) {
+if (i != 8 || j != 9)
+{
 putchar(',');
 putchar(' ');
 }
