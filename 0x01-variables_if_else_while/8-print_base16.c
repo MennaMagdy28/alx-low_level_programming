895d9176289710c939b373a0637eb513a237bb74
Menna Magdy <menna11hassanien@gmail.com>
2023-07-14 20:23:08 +0300

    k

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
 *main - started
-*Description: 
-*Return:
+*Description: hexa
+*Return: Always 0 (success)
 */
 int main(void)
 {
-
+int i;
+for (i = 0; i < 10; i++)
+putchar(i + '0');
+for (i = 0; i < 6; i++)
+putchar(i + 'a');
+putchar('\n');
+return (0);
 }
