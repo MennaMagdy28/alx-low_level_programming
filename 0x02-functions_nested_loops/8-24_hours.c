87c90d180e38bc1155a4447181da8e29a955ef23
Menna Magdy <menna11hassanien@gmail.com>
2023-07-18 20:01:41 +0300

    k

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,12 +10,12 @@ for (h = 0; h < 24; h++)
 {
 for (m = 0; m < 60; m++)
 {
-_putchar(h / 10 + '0')
-_putchar(h % 10 + '0')
-_putchar(':')
-_putchar(m / 10 + '0')
-_putchar(m % 10 + '0')
-_putchar('\n')
+_putchar(h / 10 + '0');
+_putchar(h % 10 + '0');
+_putchar(':');
+_putchar(m / 10 + '0');
+_putchar(m % 10 + '0');
+_putchar('\n');
 }
 }
 }
