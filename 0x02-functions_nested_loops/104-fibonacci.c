95ac772e52368a148f6adddec68e8db68824ea7f
Menna Magdy <menna11hassanien@gmail.com>
2023-07-18 23:34:32 +0300

    k

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,5 +7,16 @@
 */
 int main(void)
 {
-
+int i;
+unsigned int a = 1, b = 2, c;
+printf("%d, %d", a, b);
+for (i = 3; i <= 98; i++)
+{
+c = a + b;
+printf(", %u", c);
+a = b;
+b = c;
+}
+printf("\n");
+return (0);
 }
