8af9398f73a85c48bcae7d1221acb5dd64f84881
Menna Magdy <menna11hassanien@gmail.com>
2023-07-18 15:42:24 +0300

    k

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,15 @@
 #include "main.h"
 /**
+*print_alphabet - print
+*/
+void print_alphabet(void)
+{
+char i;
+for (i = 97; i <= 122; i++)
+_putchar(i);
+_putchar('\n');
+}
+/**
 *main - check the code
 *
 *Return: Always 0
