d7c8f07f7b346cd1a8aa27a3f64470cd6126cb18
Menna Magdy <menna11hassanien@gmail.com>
2023-07-18 15:50:47 +0300

    k

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
-#define main
+#ifndef MAIN_
+#define MAIN_
 void _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -12,4 +13,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
+#endif
