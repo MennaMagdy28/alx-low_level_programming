6a0622dbe92b9bd79abf9d35c258271d9709106e
Menna Magdy <menna11hassanien@gmail.com>
2023-07-18 20:20:45 +0300

    k

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*add - add
+*@a :the argument
+*@b :the argument
+*Return: sum
+*/
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,8 @@ for (i = 0; i < 10 ; i++)
 {
 for(j = 0; j < 10; j++)
 {
-_putchar(j + i + '0');
+_putchar('0');
+_putchar(i + '0');
 _putchar(',');
 _putchar(' ');
 }
