0d6e93e54695c534ad3f31c787a4e9864ab30699
Menna Magdy <menna11hassanien@gmail.com>
2023-07-18 19:38:21 +0300

    k

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 /**
 *_isalpha - check lower case
@@ -7,6 +8,8 @@
 int _isalpha(int c)
 {
 if (c >= 65 && c <= 90)
+return (1);
+else if (c >= 97 && c <= 122)
 return (1);
 else
 return (0);
