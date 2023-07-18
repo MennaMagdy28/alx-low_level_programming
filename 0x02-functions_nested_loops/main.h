8eda28b4ca5391b5c5f35ae70986369a10145e83
Menna Magdy <menna11hassanien@gmail.com>
2023-07-18 19:35:18 +0300

    k

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,8 +3,8 @@
 void _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int islower(int c);
-int isalpha(int c);
+int _islower(int c);
+int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
