0f8861faeea5877e88dd695676fd0896f6582054
Menna Magdy <menna11hassanien@gmail.com>
2023-07-18 15:09:21 +0300

    k

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#inclue "main.h"
+/**
+*main - check the code
+*Description: print putchar
+*Return: Always 0 
+*/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
