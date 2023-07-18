9673950ebc81757b7aa636e3b3a4676883b32c8c
Menna Magdy <menna11hassanien@gmail.com>
2023-07-18 22:17:19 +0300

    k

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,37 +1,19 @@
 #include "main.h"
-
 /**
- * print_to_98 - prints all natural numbers from n to 98
- * @n: starting number
- */
+* print_to_98 - prints all natural numbers from n to 98
+* @n: starting number
+*/
 void print_to_98(int n)
 {
 int i, digit;
-if (n <= 98)
+if (n > 98)
 {
-for (i = n; i < 98; i++)
-{
-digit = i / 10;
-if (digit != 0)
-_putchar(digit + '0');
-_putchar(i % 10 + '0');
-_putchar(',');
-_putchar(' ');
-}
+for (i = n; i > 98; count--)
+printf("%d, ", count);    
 }
 else
 {
-for (i = n; i > 98; i--)
-{
-digit = i / 10;
-if (digit != 0)
-_putchar(digit + '0');
-_putchar(i % 10 + '0');
-_putchar(',');
-_putchar(' ');
+for (i = n; i <= 98; count++)
+printf("%d, ",count);
 }
-}
-_putchar('9');
-_putchar('8');
-_putchar('\n');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_
 #define MAIN_
+#include <stdio.h>
 void _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
