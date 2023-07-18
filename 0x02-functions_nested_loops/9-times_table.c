46b608ca020241af72d0d96f884a0730066c7a08
Menna Magdy <menna11hassanien@gmail.com>
2023-07-18 20:28:22 +0300

    k

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,16 +4,30 @@
 */
 void times_table(void)
 {
-int i, j;
-for (i = 0; i < 10 ; i++)
+int i, j, p;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+product = i * j;
+if (j == 0)
 {
 _putchar('0');
-_putchar(' ');
-for(j = 0; j < 10; j++)
+}
+else if (product < 10)
 {
-_putchar(i + j + '0');
 _putchar(',');
 _putchar(' ');
+_putchar(' ');
+_putchar(p + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(p / 10 + '0');
+_putchar(p % 10 + '0');
+}
 }
 _putchar('\n');
 }
