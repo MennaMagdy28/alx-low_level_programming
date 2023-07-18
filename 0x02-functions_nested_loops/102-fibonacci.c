60f40163c5439e11bb04627554d284f44c5fa39b
Menna Magdy <menna11hassanien@gmail.com>
2023-07-18 23:25:07 +0300

    k

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - started
+*Description: fibo
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+int i;
+long fib[50];
+fib[0] = 1;
+fib[1] = 2;
+for (i = 2; i < 50; i++)
+{
+fib[i] = fib[i - 1] + fib[i - 2];
+}
+for (i = 0; i < 49; i++)
+{
+printf("%ld, ", fib[i]);
+}
+printf("%ld\n", fib[49]);
+return (0);
+}
