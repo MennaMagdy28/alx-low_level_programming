0e83b4f158ad991e593757b1f0d4e43e211c116b
Menna Magdy <menna11hassanien@gmail.com>
2023-07-18 13:16:57 +0300

    k

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - started
+*Description: print putchar
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
