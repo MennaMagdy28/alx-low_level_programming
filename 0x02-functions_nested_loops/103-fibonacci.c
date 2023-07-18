9f2b571501f8f2844d7741c7e33ed59c0f1a99db
Menna Magdy <menna11hassanien@gmail.com>
2023-07-18 23:31:22 +0300

    k

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,7 +7,6 @@
 */
 int main(void)
 {
-int i;
 long fib[3] = {1, 2, 0};
 long sum = 2;
 while (fib[2] < 4000000)
---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - started
+*Description: fibo
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+
+}
