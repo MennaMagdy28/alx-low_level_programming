7a7d574124743dc9349a2667b0ff0586bcd916c4
Menna Magdy <menna11hassanien@gmail.com>
2023-07-19 02:27:10 +0300

    k

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <time.h>
+/**
+*positive_or_negative - started
+*@i : parameter
+*/
+void positive_or_negative(int i)
+{
+if (i > 0)
+printf("%d is positive\n", i);
+else if (i == 0)
+printf("%d is zero\n", i);
+else
+printf("%d is negative\n", i);
+}
