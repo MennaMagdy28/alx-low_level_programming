8921fcf76daa90e22fd9e863a392da7605789eb7
Menna Magdy <menna11hassanien@gmail.com>
2023-07-18 22:19:35 +0300

    k

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,15 +5,15 @@
 */
 void print_to_98(int n)
 {
-int i, digit;
+int i;
 if (n > 98)
 {
 for (i = n; i > 98; i--)
-printf("%d, ", i);    
+printf("%d, ", i);
 }
 else
 {
 for (i = n; i <= 98; i++)
-printf("%d, ",i);
+printf("%d, ", i);
 }
 }
