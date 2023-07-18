f9792efa10ea27de0afc57e957bd3dd90356192f
Menna Magdy <menna11hassanien@gmail.com>
2023-07-18 19:51:59 +0300

    k

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*print_last_digit - check lower case
+*@num :the argument
+*Return: last digit of num
+*/
+int print_last_digit(int num)
+{
+int digit;
+digit = num % 10;
+return (digit);
+}
