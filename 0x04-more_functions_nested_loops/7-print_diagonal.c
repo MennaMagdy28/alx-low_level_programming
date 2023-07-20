e222551959874d75d179b65909410d678526bd3f
Menna Magdy <menna11hassanien@gmail.com>
2023-07-20 17:19:04 +0300

    k

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,17 +5,17 @@
 */
 void print_diagonal(int n)
 {
+int i, j;
 if (n <= 0)
 _putchar('\n');
 else
 {
-while (n--)
+for (i = 0; i < 10; i++)
 {
-while (n--)
+for (j = 0; j < n; j++)
 _putchar(' ');
-
-}
 _putchar('\\');
 _putchar('\n');
+}
 }
 }
