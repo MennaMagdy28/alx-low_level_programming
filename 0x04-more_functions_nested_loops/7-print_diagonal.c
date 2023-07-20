40f5a68c6d7e755b4b8b7d8461983d2f6cddc70e
Menna Magdy <menna11hassanien@gmail.com>
2023-07-20 17:23:44 +0300

    k

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,9 +10,9 @@ if (n <= 0)
 _putchar('\n');
 else
 {
-for (i = 0; i < 10; i++)
+for (i = 1; i <= n; i++)
 {
-for (j = 0; j < n; j++)
+for (j = 1; j <= n; j++)
 _putchar(' ');
 _putchar('\\');
 _putchar('\n');
