f9f0cb91491251e810bfc24c0bb98c84189c5f40
Menna Magdy <menna11hassanien@gmail.com>
2023-07-20 17:48:59 +0300

    k

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,7 +5,7 @@
 *Description : fizzbuzz
 *Return : 0 success
 */
-int main()
+int main(void)
 {
 for (int i = 1; i <= 100; i++)
 {
@@ -15,9 +15,11 @@ else if (i % 3 == 0)
 printf("Fizz ");
 else if (i % 5 == 0)
 printf("Buzz ");
+else if(i == 100)
+printf("%d", i);
 else
 printf("%d ", i);
 }
 printf("\n");
-return 0;
+return (0);
 }
