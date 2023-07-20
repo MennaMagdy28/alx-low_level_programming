364f9ac87ae1e5610f172baff8ca28cea1f12118
Menna Magdy <menna11hassanien@gmail.com>
2023-07-20 23:58:00 +0300

    k

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,7 +9,7 @@ double _sqrt(double x)
 float sqrt, tmp;
 sqrt = x / 2;
 tmp = 0;
-while(sqrt != tmp)
+while (sqrt != tmp)
 {
 tmp = sqrt;
 sqrt = (x / tmp + tmp) / 2;
@@ -27,7 +27,7 @@ while (num % 2 == 0)
 num = num / 2;
 for (prm = 3; prm <= _sqrt(num); prm += 2)
 {
-while(num % prm == 0)
+while (num % prm == 0)
 {
 num = num / prm;
 largest = prm;
