41fc83284bf7ac202c794816e4b4900c2ce58fd0
Menna Magdy <menna11hassanien@gmail.com>
2023-07-21 00:04:46 +0300

    k

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_number - print number
+*@n : num
+*/
+void print_number(int n)
+{
+unsigned int num;
+num = n;
+if (n < 0)
+{
+_putchar('-');
+num = -num;
+}
+if ((num / 10) > 10)
+print_number(num / 10);
+_putchar((num % 10) + 48);
+}
+x
