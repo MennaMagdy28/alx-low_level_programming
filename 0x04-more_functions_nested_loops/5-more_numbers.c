49db18c1f2cf0cdf4ea94d329d4845650ab08bd4
Menna Magdy <menna11hassanien@gmail.com>
2023-07-20 15:25:23 +0300

    k

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,11 +4,19 @@
 */
 void more_numbers(void)
 {
-int a;
-a = 0
-while(a < 15)
+int a, r, c;
+for (r = 1; r <= 10; r++)
 {
-printf("%d", a);
+for (c = 0; c <= 14; c++)
+{
+a = c;
+if (c > 9)
+{
+_putchar(1 + 48);
+a = c % 10;
+}
+_putchar(a + 48);
 }
 _putchar('\n');
+}
 }
