df80ce2239324faceda391cbbc6d59837f9b1c46
Menna Magdy <menna11hassanien@gmail.com>
2023-07-20 17:13:36 +0300

    k

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,17 +1,9 @@
 #include "main.h"
 /**
 *print_square - print
-*@n : lenght
+*@size : lenght
 */
 void print_square(int size)
 {
-if (n > 0)
-{
-while (n--)
-{
-while (true)
-_putchar('#');
-}
-}
-_putchar('\n');
+
 }
