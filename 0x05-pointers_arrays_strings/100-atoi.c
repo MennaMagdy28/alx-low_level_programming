45464a7fe0becb3abc268b69ffae9c99cea3366a
Menna Magdy <menna11hassanien@gmail.com>
2023-07-24 23:54:40 +0300

    k

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,7 +13,7 @@ int _atoi(char *s)
 		if (*s == '-')
 			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
-			num = (num + 10) + (*s - '0');
+			num = (num * 10) + (*s - '0');
 		else if (num > 0)
 			break;
 	} while (*s++);
