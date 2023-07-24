c04f378fa34749a2fe7e9c61b9fd451625731b07
Menna Magdy <menna11hassanien@gmail.com>
2023-07-24 21:01:02 +0300

    k

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_rev - string length
+* @s: the string
+*/
+void print_rev(char *s)
+{
+	int i;
+
+	i = _strlen(*s);
+	while (i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
