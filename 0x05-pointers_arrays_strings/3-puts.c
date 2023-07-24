9da57df4efe3adf5c0e0796c2ec0ed203cf03e62
Menna Magdy <menna11hassanien@gmail.com>
2023-07-24 20:52:50 +0300

    k

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _puts - string print
+* @str: the string
+*/
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
