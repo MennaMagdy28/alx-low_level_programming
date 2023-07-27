2f9f58c293ccbe2118dde985408119cceee12281
Menna Magdy <menna11hassanien@gmail.com>
2023-07-27 22:45:51 +0300

    k

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,9 +2,19 @@
 
 /**
 * string_toupper - make upper case
+* @s: string
 * Return: pointer to dest
 */
-char *string_toupper(char *)
+char *string_toupper(char *s)
 {
+	int i;
 
+	i = 0;
+	while (s[i])
+	{
+		if (*s >= 97 && *s <= 122)
+			*s -= 32;
+		i++;
+	}
+	return (s);
 }
