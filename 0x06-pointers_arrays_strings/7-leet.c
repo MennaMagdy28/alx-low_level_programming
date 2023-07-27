72c2996aa9198891a1b7f67d761674713d171fe5
Menna Magdy <menna11hassanien@gmail.com>
2023-07-28 02:14:09 +0300

    k

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,9 +2,25 @@
 
 /**
 * leet - incode str
+* @c: string
 * Return: pointer to dest
 */
-char *leet(char *)
+char *leet(char *c)
 {
+	char *cp = c;
+	char decode[] = {'A', 'E', 'O', 'T', 'L'};
+	int incode[] = {4, 3, 0, 7, 1};
+	unsigned int i;
+
+	while (*c)
+	{
+		for (i = 0; i < sizeof(decode) / sizeof(char); i++)
+		{
+			if (*c == decode[i] || *c == decode[i] + 32)
+				*c = 48 + incode[i];
+		}
+		c++;
+	}
+	return (cp);
 
 }
