5b235d033a0ffe75fcd1eb6f4b3d985994857661
Menna Magdy <menna11hassanien@gmail.com>
2023-07-28 02:32:58 +0300

    k

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,9 +3,17 @@
 /**
 *print_number - print num
 * @n: num
-* Return: pointer to dest
 */
 void print_number(int n);
 {
-
+	unsigned int num;
+	num = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	if (num / 10 != 0)
+		print_number(num / 10);
+	_putchar((n % 10) + '0');
 }
