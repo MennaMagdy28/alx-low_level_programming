e931cde8007fcf71a8ea9df214243e6f8ee2a157
Menna Magdy <menna11hassanien@gmail.com>
2023-07-27 21:54:09 +0300

    k

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,9 +4,24 @@
 * _strcat - get src str _ concatenate with dest str _ src=new var=new
 * @dest: first string
 * @src: second string
-* Return: pointer to dest 
+* Return: pointer to dest
 */
 char *_strcat(char *dest, char *src)
 {
+	int i, j, ctr, tmp;
 
+	i = 0;
+	j = 0;
+	tmp = 0;
+	while (src[i] != '\0')
+		i++;
+	while (dest[j] != '\0')
+		j++;
+	for (ctr = i; ctr < i + j; ctr++)
+	{
+		dest[ctr] = src[tmp];
+		tmp++;
+	}
+	dest[i + j] = '\0';
+	return (dest[0]);
 }
