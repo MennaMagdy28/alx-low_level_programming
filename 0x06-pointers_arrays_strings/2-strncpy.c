629dd7d77cbe27363df5b415dbae84b27d6375d6
Menna Magdy <menna11hassanien@gmail.com>
2023-07-27 22:22:46 +0300

    k

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,17 @@
 #include "main.h"
 
 /**
-* _strncopy - copy first n chars to the str/str = new
+* _strncpy - copy first n chars to the str/str = new
 * @dest: first string
 * @src: second string
+* @n: size
 * Return: pointer to dest
 */
-char *_strncopy(char *dest, char *src)
+char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
 }
