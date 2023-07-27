9cde0d1cb90e69a33ada3e744127fc1899d5ea4f
Menna Magdy <menna11hassanien@gmail.com>
2023-07-28 02:26:06 +0300

    k

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,10 +1,29 @@
 #include "main.h"
 
 /**
-* cap_string - don't understand yet
+* rot13 - don't understand yet
+* @str: string
 * Return: pointer to dest
 */
-char *cap_string(char *);
+char *rot13(char *str);
 {
+	char *ptr = str;
+	int i;
+	char decode[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char incode[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
+
+	while (*s)
+	{
+		for (i = 0; i <= 52; i++)
+		{
+			if (*s == decode[i])
+			{
+				*s = incode[i];
+				break;
+			}
+		}
+		s++;
+	}
+	return (ptr);
 }
