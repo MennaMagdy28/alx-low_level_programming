bbe09b834534ab4cfc553d2acdf1bef7f0299566
Menna Magdy <menna11hassanien@gmail.com>
2023-07-27 22:35:33 +0300

    k

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,5 +8,18 @@
 */
 int _strcmp(char *s1, char *s2)
 {
+	int sum1, sum2, i, j;
 
+	sum1 = 0;
+	while (*s1)
+	{
+		if (*s1 != *s2)
+		{
+			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
+		s1++;
+		s2++;
+	}
+	return (equal);
 }
