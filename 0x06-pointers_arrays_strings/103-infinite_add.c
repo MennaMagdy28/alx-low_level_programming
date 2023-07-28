da4419bfaf6187509e8398cee623ef942d082a6e
Menna Magdy <menna11hassanien@gmail.com>
2023-07-28 04:15:55 +0300

    k

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,14 +1,56 @@
 #include "main.h"
 
 /**
-* infinite_add _ add add add add 
+* infinite_add - add two big numbers
 * @n1: first string
 * @n2: second string
 * @r:buffer
-* @size_r: buffer size 
+* @size_r: buffer size
 * Return: pointer to dest
 */
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 {
+	int i, j, k, l, f, s, d;
 
+	i = 0;
+	j = 0;
+	l = 0;
+	d = 0;
+
+	while (n1[i] != '\0')
+		i++;
+	while (n2[j] != '\0')
+		j++;
+	if (i > j)
+		l = i;
+	else
+		l = j;
+	if (l + 1 > size_r)
+		return (0);
+	r[l] = '\0';
+	for (k = l - 1; k >= 0; k--)
+	{
+		i--;
+		j--;
+		if (i >= 0)
+			f = n1[i] - '0';
+		else
+			f = 0;
+		if (j >= 0)
+			s = n2[j] - '0';
+		else
+			s = 0;
+		r[k] = (f + s + d) % 10 + '0';
+		d = (f + s + d) / 10;
+	}
+	if (d == 1)
+	{
+		r[l + 1] = '\0';
+		if (l + 2 > size_r)
+			return (0);
+		while (l-- >= 0)
+			r[l + 1] = r[l];
+		r[0] = d + '0';
+	}
+	return (r);
 }
