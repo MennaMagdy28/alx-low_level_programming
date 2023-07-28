abb5f5ac042ddfdfb0be8a8e819e677f69339e3e
Menna Magdy <menna11hassanien@gmail.com>
2023-07-28 04:37:12 +0300

    k

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,5 +1,54 @@
 #include "main.h"
+/**
+ * valid - valid
+ * @n: integer
+ * Return: 1 true, 0 false*
+*/
+int valid(int n)
+{
+	return (n >= 32 && n <= 126);
+}
+/**
+ * hex - print hex
+ * @b: string
+ * @start: starting
+ * @end: ending
+*/
+void hex(char *b, int start, int end)
+{
+	int i;
 
+	i = 0;
+	while (i < 10)
+	{
+		if (i < end)
+			printf("%02x", *(b + start + i));
+		else
+			printf("  ");
+		if (i % 2)
+			printf(" ");
+		i++;
+	}
+}
+/**
+ * ascii - print ascii
+ * @b: string
+ * @start: starting
+ * @end: ending
+*/
+void ascii(char *b, int start, int end)
+{
+	int i = 0, ch;
+
+	while (i < end)
+	{
+		ch = *(b + i + start);
+		if (!valid(ch))
+			ch = 46;
+		printf("%c", ch);
+		i++;
+	}
+}
 /**
 * print_buffer - didn't understand yet
 * @b: buffer
@@ -7,5 +56,19 @@
 */
 void print_buffer(char *b, int size)
 {
+	int start, end;
 
+	if (size > 0)
+	{
+		for (start = 0; sttart < size; start += 10)
+		{
+			end = (size - start < 10)  ? size - start : 10;
+			printf("%08x: ", start);
+			hex(b, start, end);
+			ascii(b, start, end);
+			printf("\n");
+		}
+	}
+	else
+		printf("\n");
 }
