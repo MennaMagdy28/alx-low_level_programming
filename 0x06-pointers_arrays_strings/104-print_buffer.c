43ea5082414732f57745536155189dfe599f7b6c
Menna Magdy <menna11hassanien@gmail.com>
2023-07-28 04:38:28 +0300

    k

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -60,7 +60,7 @@ void print_buffer(char *b, int size)
 
 	if (size > 0)
 	{
-		for (start = 0; sttart < size; start += 10)
+		for (start = 0; start < size; start += 10)
 		{
 			end = (size - start < 10)  ? size - start : 10;
 			printf("%08x: ", start);
