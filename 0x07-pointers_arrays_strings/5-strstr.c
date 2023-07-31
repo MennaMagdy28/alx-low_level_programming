9f2335fbef685cbbd274ada4f372826fd75da600
Menna Magdy <menna11hassanien@gmail.com>
2023-08-01 00:42:36 +0300

    k

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,29 @@
 #include "main.h"
 /**
-* function -
-* @
-* Return:
+* _strstr - strstr
+* @haystack: first string
+* @needle: substring
+* Return: pointer
 */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, k;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0, k = i; needle[j] != '\0'; j++, k++)
+			{
+				if (needle[j] != haystack[k])
+					break;
+			}
+		}
+		if (needle[j] == '\0')
+		{
+			return (haystack + i);
+		}
+	}
+	return (NULL);
+}
