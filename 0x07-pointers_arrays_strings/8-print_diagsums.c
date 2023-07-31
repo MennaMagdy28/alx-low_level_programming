903016260ebcc1063040349b34ef531c5a54ca4b
Menna Magdy <menna11hassanien@gmail.com>
2023-08-01 01:41:35 +0300

    k

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
 * print_diagsums - diagsums
 * @a: string
@@ -7,12 +8,13 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, s1 = 0, s2 = 0;
-
+	int i, j, s1 = 0, s2 = 0;
+	
+	j = size - 1;
 	for (i = 0; i < size; i++)
 	{
 		s1 += a[i];
-		s2 += a[size - i - 1];
+		s2 += a[j - i];
 		a += size;
 	}
 	printf("%d, ", s1);
