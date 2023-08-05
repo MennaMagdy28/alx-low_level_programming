b3efce613977c3628bd752f93e70a19e0a50ea17
Menna Magdy <menna11hassanien@gmail.com>
2023-08-05 03:54:41 +0300

    k

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print program name
+ * @argc: size
+ * @argv: list
+ * Return: 0
+*/
+
+int main (int argc, char const **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
