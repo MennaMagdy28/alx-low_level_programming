69da2e295376eba0471dda7a266c81865db8beed
Menna Magdy <menna11hassanien@gmail.com>
2023-08-05 04:25:15 +0300

    k

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,10 +19,11 @@ int main(int argc, char **argv)
 		{
 			if (*c < '0' || *c > '9')
 			{
-				return (printf("Error\n"), 1);
+				printf("Error\n");
+				return (1);
 			}
-			sum += atoi(argv[argc]);
 		}
+		sum += atoi(argv[argc]);
 	}
 	printf("%d\n", sum);
 	return (0);
