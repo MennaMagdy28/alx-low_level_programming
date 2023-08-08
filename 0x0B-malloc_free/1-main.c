ec862a839d830133f7d8862711b0d0f118eedaaa
Menna Magdy <menna11hassanien@gmail.com>
2023-08-08 05:32:55 +0300

    k

--- 0x0B-malloc_free/1-main.c
+++ 0x0B-malloc_free/1-main.c
@@ -11,7 +11,7 @@ int main(void)
 {
     char *s;
 
-    s = _strdup("ALX SE");
+    s = _strdup("menna did the task right");
     if (s == NULL)
     {
         printf("failed to allocate memory\n");
--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
 /**
+ * len - strlen
+ * @str:string
+ * Return: int
+*/
+unsigned int len(char *str)
+{
+	unsigned int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
  * _strdup -  copy of the string given as a parameter.
  * @str: string
  * Return: pointer or NULL
@@ -8,12 +22,12 @@
 
 char *_strdup(char *str)
 {
-	char *ptr = (char *) malloc(sizeof(str));
-	long unsigned int i;
+	unsigned int i, j = len(str);
+	char *ptr = malloc(sizeof(*str) * j + 1);
 
-	if (ptr == 0)
+	if (ptr == 0 || str == NULL)
 		return (NULL);
-	for (i = 0; i < sizeof(str); i++)
+	for (i = 0; i < j; i++)
 		ptr[i] = str[i];
 	return (ptr);
 
