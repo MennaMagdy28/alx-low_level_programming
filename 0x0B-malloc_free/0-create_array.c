a99e48f6ea27ed338a5342da4efe2c453a0f1852
Menna Magdy <menna11hassanien@gmail.com>
2023-08-08 04:59:31 +0300

    k

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@ char *create_array(unsigned int size, char c)
 {
 	char *ptr = (char *) malloc(size * sizeof(char));
 
-	if (size == 0)
+	if (size == 0 || ptr == 0)
 		return (NULL);
 
 	while (size != 0)
