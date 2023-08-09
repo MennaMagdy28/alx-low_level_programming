9b80b1de0c6d05cff7e56c9086f885ab0a3468f9
Menna Magdy <menna11hassanien@gmail.com>
2023-08-09 03:55:52 +0300

    k

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * len - strlen
+ * @str:string
+ * Return: int
+*/
+
+int len(char *str)
+{
+	int i = 0;
+	
+	if (str == NULL)
+		return (0);
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * argstostr -  arguments to string
+ * @ac: int
+ * @av: arguments
+ * Return: string
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, nc = 0, cmpt = 0;
+	char *ptr;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++, nc++)
+		nc += len(av[i]);
+
+	ptr = malloc(sizeof(char) * nc + 1);
+	if (ptr == 0)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, cmpt++)
+			ptr[cmpt] = av[i][j];
+		ptr[cmpt] = '\n';
+		cmpt++;
+	}
+	ptr[cmpt] = '\0';
+	return (ptr);
+}
