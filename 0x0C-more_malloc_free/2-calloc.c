6b7538725f203fefbc8bd09de39f22986e5a2980
Menna Magdy <menna11hassanien@gmail.com>
2023-08-10 17:43:47 +0300

    k

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _memset - memset
+ * @c: char
+ * @n: size
+ * @s: string
+ * Return: pointer
+*/
+
+char *_memset(char *s, char c, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+		*s++ = c;
+	return (ptr);
+}
+/**
+ * _calloc - calloc
+ * @nmemb: int
+ * @size: size
+ * Return: pointer
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr = malloc(size * nmemb);
+	unsigned int total = nmemb * size;
+
+	if (size == 0 || nmemb == 0 || ptr == 0)
+		return (NULL);
+
+	_memset(ptr, 0, total);
+	return (ptr);
+}
