42740a47b8cce0b11ac8e699b3bb738f4cea9a81
Menna Magdy <menna11hassanien@gmail.com>
2023-08-10 19:58:49 +0300

    k

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
- * malloc_checked - check on malloc
- * @b: size
+ * array_range - array of integrs
+ * @min: start
+ * @max: end
+ * Return: array
 */
+
+int *array_range(int min, int max)
+{
+	int len = max - min + 1, i = 0, *ptr;
+
+	if (min > max)
+		return (NULL);
+	ptr = malloc(sizeof(int) * len);
+	if (ptr == 0)
+		return (NULL);
+	for (; i < len; i++)
+		ptr[i] = min++;
+	return (ptr);
+
+}
