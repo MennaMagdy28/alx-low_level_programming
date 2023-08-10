8b2514e6fff67901d03ca678591a1cdca4e1c5b1
Menna Magdy <menna11hassanien@gmail.com>
2023-08-10 13:53:23 +0300

    k

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,12 +3,13 @@
 /**
  * malloc_checked - check on malloc
  * @b: size
+ * Return: pointer
 */
 
 void *malloc_checked(unsigned int b)
 {
 	int *ptr = malloc(b);
-	
+
 	if (ptr == 0)
 		exit(98);
 	return (ptr);
