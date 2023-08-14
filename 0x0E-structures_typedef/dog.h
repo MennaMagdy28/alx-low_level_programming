67192745b03812c8c297cc89c8fc11e64ca36e71
Menna Magdy <menna11hassanien@gmail.com>
2023-08-14 16:29:37 +0300

    k

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG 
+
+/**
+ * struct dog - a dog struct 
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name 
+ *
+ * Description: sturcture
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
