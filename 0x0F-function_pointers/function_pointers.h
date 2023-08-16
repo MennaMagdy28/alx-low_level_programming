37559b1d93cb8d3436c2167165537c2e607b1014
Menna Magdy <menna11hassanien@gmail.com>
2023-08-16 17:23:16 +0300

    k

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef MAIN
+#define MAIN
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+void print_name(char *name, void (*f)(char *));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
