8d20c85954ef8e18ca7acb6df43b9c5f08b4cd0f
Menna Magdy <menna11hassanien@gmail.com>
2023-08-25 11:09:27 +0300

    k

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,5 +8,5 @@ void first(void) __attribute__ ((constructor));
 */
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my bacl!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my bacK!\n");
 }
