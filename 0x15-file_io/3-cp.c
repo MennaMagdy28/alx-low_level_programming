8232d47d2384af25b1523501ef3e6c1d13f882ee
Menna Magdy <menna11hassanien@gmail.com>
2023-09-05 00:17:42 +0300

    k

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,11 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * error_file - checks if files can be opened.
- * @file_from: file_from
- * @file_to: file_to
+ * @file_from: file_from.
+ * @file_to: file_to.
  * @argv: arguments vector.
-*/
+ * Return: no return.
+ */
 void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
@@ -19,12 +21,13 @@ void error_file(int file_from, int file_to, char *argv[])
 		exit(99);
 	}
 }
+
 /**
  * main - check the code for Holberton School students.
- * @argc: number
- * @argv: arguments
+ * @argc: number of arguments.
+ * @argv: arguments vector.
  * Return: Always 0.
-*/
+ */
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
