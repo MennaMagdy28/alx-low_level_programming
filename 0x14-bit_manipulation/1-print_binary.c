efbce6d3e387a6d6e70f356cf9eaffe656ae5cd6
Menna Magdy <menna11hassanien@gmail.com>
2023-11-23 18:07:39 +0200

    k

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_binary - convert to binary then print
+ * @n: num
+*/
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n / 2);
+	_putchar(n % 2 + '0');
+}
+
