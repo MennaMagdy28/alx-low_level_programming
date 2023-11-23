71147b9299356fac07b8057525294dea20b72311
Menna Magdy <menna11hassanien@gmail.com>
2023-11-23 16:28:03 +0200

    k

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,18 +17,18 @@ return (x);
 }
 
 /**
- * _pow - power function
- * @num: number
- * @p: power
- * Return: int num
- */
+* _pow - power function
+* @num: number
+* @p: power
+* Return: int num
+*/
 unsigned int _pow(int num, int p)
 {
-	int res = 1, i;
+int res = 1, i;
 
-	for (i = 0; i < p; i++)
-		res *= num;
-	return (res);
+for (i = 0; i < p; i++)
+res *= num;
+return (res);
 }
 
 /**
@@ -38,23 +38,25 @@ unsigned int _pow(int num, int p)
 */
 unsigned int binary_to_uint(const char *b)
 {
-	int size = Slen(b) - 1, res = 0, i;
+int size = Slen(b) - 1, res = 0, i;
 
-	for (i = 0; i < size + 1; i++)
-	{
-		if (*b == '0')
-			b++;
-		else if (*b == '1')
-		{
-			b++;
-			res += _pow(2, size - i);
-		}
-		else
-		{
-			res = 0;
-			break;
-		}
-	}
-	return (res);
+if (!b)
+	return (0);
+for (i = 0; i < size + 1; i++)
+{
+if (*b == '0')
+b++;
+else if (*b == '1')
+{
+b++;
+res += _pow(2, size - i);
+}
+else
+{
+res = 0;
+break;
+}
+}
+return (res);
 
 }
