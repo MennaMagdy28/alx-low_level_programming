a4d107b53106817e26a95df8d60320a823091b26
Menna Magdy <menna11hassanien@gmail.com>
2023-11-23 20:18:13 +0200

    k

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,7 +10,6 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int cnt = 0;
 	unsigned long int temp = n ^ m;
-	int i = 0;
 
 	for (; temp > 0; temp = temp >> 1)
 	{
