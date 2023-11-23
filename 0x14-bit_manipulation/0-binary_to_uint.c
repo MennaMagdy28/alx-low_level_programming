08749da7477084ba2de085ec0fc76fefe22dc1cc
Menna Magdy <menna11hassanien@gmail.com>
2023-11-23 16:03:18 +0200

    k

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,32 +1,60 @@
-//
-// Created by Menna Magdy on 11/23/2023.
-//
-#include "main.h"
-#include <stdio.h>
+#include"main.h"
 
 /**
- * Slen - a function to get string length
- * @s: string
- * Return: int length
- */
+* Slen - a function to get string length
+* @s: string
+* Return: intlength
+*/
 unsigned int Slen(const char *s)
 {
-    int x = 0;
-    while(*s != '\0')
-    {
-        x++;
-        s++;
-    }
-    printf("%d", x);
-    return (x);
+int x = 0;
+while (*s != '\0')
+{
+x++;
+s++;
+}
+return (x);
 }
 
 /**
-* binary_to_unit - a function to convert binary to decimal
- * @b: a string contains 0s 1s => binary
- * Return: integer num => decimal
+ * _pow - power function
+ * @num: number
+ * @p: power
+ * Return: int num
+ */
+unsigned int _pow(int num, int p)
+{
+	int res = 1;
+
+	for (int i = 0; i < p; i++)
+		res *= num;
+	return (res);
+}
+
+/**
+* binary_to_uint - a function to convert binary to decimal
+* @b: a string contains 0s 1s => binary
+* Return: integer num => decimal
 */
 unsigned int binary_to_uint(const char *b)
 {
+	int size = Slen(b) - 1, res = 0;
+
+	for (int i = 0; i < size + 1; i++)
+	{
+		if (*b == '0')
+			b++;
+		else if (*b == '1')
+		{
+			b++;
+			res += _pow(2, size - i);
+		}
+		else
+		{
+			res = 0;
+			break;
+		}
+	}
+	return (res);
 
 }
