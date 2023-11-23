0858efdb98814dc0caf99c763cf8185fc6985542
Menna Magdy <132995770+MoroX11@users.noreply.github.com>
2023-11-23 15:40:54 +0200

    Add files via upload

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,18 @@
+//
+// Created by Menna Magdy on 11/23/2023.
+//
+
+#ifndef ALX_LOW_LEVEL_PROGRAMMING_MAIN_H
+#define ALX_LOW_LEVEL_PROGRAMMING_MAIN_H
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
+#endif //ALX_LOW_LEVEL_PROGRAMMING_MAIN_H
+
+
