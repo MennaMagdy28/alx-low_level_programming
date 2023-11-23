baf0a3a6645cb4d9a8bb1771de06ef388852d646
Menna Magdy <menna11hassanien@gmail.com>
2023-11-23 19:27:11 +0200

    k

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,6 +8,9 @@
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-*n += (1 << index);
-return (*n);
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	*n += (1 << index);
+	return (1);
 }
