4fbe37089dd5c3ee976ea3a2a755b663e9579414
Menna Magdy <menna11hassanien@gmail.com>
2023-11-23 19:59:59 +0200

    k

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * clear_bit - to set a bit to 0
+ * @n: refrence
+ * @index: where to put 0
+ * Return: 1 -1
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (1);
+	*n = *n & ~(1 << index);
+	return (1);
+}
