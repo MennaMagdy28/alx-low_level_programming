d23cfe4d952906193418d80643335cfb21fbbbd3
Menna Magdy <menna11hassanien@gmail.com>
2023-11-23 16:57:23 +0200

    k

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,6 +9,11 @@ unsigned int Slen(const char *s)
 {
 int x = 0;
 
+<<<<<<< HEAD
+=======
+if (!s)
+return (0);
+>>>>>>> 641ff1e (k)
 while (*s != '\0')
 {
 x++;
@@ -41,21 +46,14 @@ unsigned int binary_to_uint(const char *b)
 {
 unsigned int size = Slen(b) - 1, i, res = 0;
 
-if (!b)
+
+for (i = 0; i < size + 1; i++, b++)
+{
+if(*b != '0' && *b != '1')
 	return (0);
-for (i = 0; i < size + 1; i++)
+else if (*b & 1)
 {
-if (*b == '0')
-b++;
-else if (*b == '1')
-{
-b++;
 res += _pow(2, size - i);
-}
-else
-{
-res = 0;
-break;
 }
 }
 return (res);
