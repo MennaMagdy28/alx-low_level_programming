bd4894baf1729a58da74a4a1878154ac5ac6116b
menna magdy <menna11hassanien@gmail.com>
2024-07-27 19:41:30 +0300

    moroX

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+
+ void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *h;
+	dlistint_t *tmp;
+
+	h = head;
+	if (h = NULL)
+		return (NULL);
+	while (h->next != NULL)
+		h = h->next;
+	while (h->prev != NULL)
+	{
+		tmp = h->prev;
+		free(h);
+		tmp->next = NULL;
+		h = tmp;
+	}
+}
