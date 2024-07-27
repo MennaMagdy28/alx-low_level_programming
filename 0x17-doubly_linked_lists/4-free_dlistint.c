884917e17752fe1fbfcd39f4d206e0461f5f32dd
menna magdy <menna11hassanien@gmail.com>
2024-07-27 19:46:46 +0300

    moroX

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -6,21 +6,18 @@
 * @head: head of the list
 * Return: no return
 */
+
 void free_dlistint(dlistint_t *head)
 {
-dlistint_t *h;
 dlistint_t *tmp;
 
-h = head;
-if (h == NULL)
-return (NULL);
-while (h->next != NULL)
-h = h->next;
-while (h->prev != NULL)
+if (head != NULL)
+while (head->prev != NULL)
+head = head->prev;
+
+while ((tmp = head) != NULL)
 {
-tmp = h->prev;
-free(h);
-tmp->next = NULL;
-h = tmp;
+head = head->next;
+free(tmp);
 }
 }
