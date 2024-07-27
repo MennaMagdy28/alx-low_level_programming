e2a075d6d7d09def86b3649e0bcf55ffa6b374ee
menna magdy <menna11hassanien@gmail.com>
2024-07-27 18:55:29 +0300

    moroX

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,12 +1,16 @@
 #include "lists.h"
-/*
-* dlistint_len - get length of the list
-* @h: pointer to the head
-* Return: length
+
+/**
+* dlistint_len - returns the number of elements in
+* a double linked list
+*
+* @h: head of the list
+* Return: the number of nodes
 */
-size_t dlistint_len(dlistint_t *h)
+
+size_t dlistint_len(const dlistint_t *h)
 {
-unsigned long cnt;
+size_t cnt;
 
 cnt = 0;
 if (h == NULL)
