15e41f48af62156ddd2a0b7ee353779062316a1b
menna magdy <menna11hassanien@gmail.com>
2024-07-27 19:43:00 +0300

    moroX

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,21 +1,26 @@
 #include "lists.h"
 
-
- void free_dlistint(dlistint_t *head)
+/**
+* free_dlistint - frees a dlistint_t list
+*
+* @head: head of the list
+* Return: no return
+*/
+void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *h;
-	dlistint_t *tmp;
+dlistint_t *h;
+dlistint_t *tmp;
 
-	h = head;
-	if (h = NULL)
-		return (NULL);
-	while (h->next != NULL)
-		h = h->next;
-	while (h->prev != NULL)
-	{
-		tmp = h->prev;
-		free(h);
-		tmp->next = NULL;
-		h = tmp;
-	}
+h = head;
+if (h == NULL)
+return (NULL);
+while (h->next != NULL)
+h = h->next;
+while (h->prev != NULL)
+{
+tmp = h->prev;
+free(h);
+tmp->next = NULL;
+h = tmp;
+}
 }
