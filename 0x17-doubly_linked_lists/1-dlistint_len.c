773b14597ac54ffdf9491ca2ee6fcebb1cef96d3
menna magdy <menna11hassanien@gmail.com>
2024-07-27 18:48:45 +0300

    moroX

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/*
+* dlistint_len - get length of the list
+* @h: pointer to the head
+* Return: length 
+*/
+size_t dlistint_len(dlistint_t *h)
+{
+    unsigned long cnt;
+
+    cnt = 0;
+    if (h == NULL)
+    return (cnt);
+    while (h->prev != NULL)
+    {
+        h = h->prev;
+    }
+    while (h != NULL)
+    {
+        cnt++;
+        h = h->next;
+    }
+    return (cnt);
+}
